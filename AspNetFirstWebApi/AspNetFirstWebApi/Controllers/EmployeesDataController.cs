2132ec5e93ccbaab1adb691c00a612a763ebf57a
Shimulbd75 <niamul.islam4623@gmail.com>
2023-08-17 11:13:35 +0600

    comments

---
+++ AspNetFirstWebApi/AspNetFirstWebApi/Controllers/EmployeesDataController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AspNetFirstWebApi.Controllers
+{
+    public class EmployeesDataController : ApiController
+    {
+        public string[] MyEmployees = { "Naba", "Nihan", "Muaz", "Noushin" };
+        public string[] GetEmployees()
+        {
+            return MyEmployees;
+        }
+        public string GetEmployeesById(int id)
+        {
+            return MyEmployees[id];
+        }
+    }
+}
